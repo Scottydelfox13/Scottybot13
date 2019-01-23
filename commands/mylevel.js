74873ecad2ba0b9baee9066d12cd48e26082bc7f
Scottydelfox13 <Scottydelfox13@gmail.com>
2019-01-22 17:43:33 -0800

    the ability to see a users level based off of a mention!

--- commands/mylevel.js
+++ commands/mylevel.js
@@ -1,11 +1,21 @@
 exports.run = async (client, message, args, level) => {
+
+const user = message.mentions.members.first(); 
+  const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.modRole.toLowerCase());
   const friendly = client.config.permLevels.find(l => l.level === level).name;
-  message.reply(`Your permission level is: ${level} - ${friendly}`);
+  if(user && user.id === client.config.ownerID) return message.channel.send(`${user.displayName}'s permission level is: 10 - Bot Owner`);
+  else if(user && client.config.admins.includes(user.id)) return message.channel.send(`${user.displayName}'s permission level is: 9 - Bot Admin`);
+  else if(user && client.config.support.includes(user.id)) return message.channel.send(`${user.displayName}'s permission level is: 8 - Bot Support`);
+  else if(user && message.guild.ownerID === user.id) return message.channel.send(`${user.displayName}'s permission level is: 4 - Server Owner`);
+  else if(user && user.hasPermission('ADMINISTRATOR')) return message.channel.send(`${user.displayName}'s permission level is: 3 - Administrator`);
+  else if(user && modRole && user.roles.has(modRole.id)) return message.channel.send(`${user.displayName}'s permission level is: 2 - Moderator`);
+  else if(user) return message.channel.send(`${user.displayName}'s permission level is: 0 - User`);
+  else return message.reply(`Your permission level is: ${level} - ${friendly}`);
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: true,
+  guildOnly: false,
   aliases: [],
   permLevel: "User"
 };
@@ -14,5 +24,5 @@ exports.help = {
   name: "mylevel",
   category: "Miscelaneous",
   description: "Tells you your permission level for the current message location.",
-  usage: "mylevel"
+  usage: "mylevel [user mention]"
 };
